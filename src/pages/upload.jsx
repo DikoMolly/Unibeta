360a41bcecbefec9e6a14d181d3132a99ebde817
DikoMolly <emmanuelnnachi26@gmail.com>
2024-09-26 15:52:40 +0100

    Added a new page called vendor

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
-
+import "../App.css"
+import arrowRight from "../assets/arrow-left.png"
+import cloud from "../assets/cloud.png"
+import Footer from "../components/Footer"
 const upload = () => {
   return (
-    <div>
-        <h1>Hello</h1>
-    </div>
+    <>
+         <div className='upload1'>
+            <div>
+                <h1> <span>+</span>Upload </h1>
+            </div>
+            <div>
+                <img src={arrowRight} alt="" />
+            </div>
+        </div>
+        <div className='upload2'>
+            <div className='box1'></div>
+            <div className='box2'></div>
+            <div className='box3'></div>
+            <div className='box4'></div>
+        </div>
+
+        <div className='upload3'>
+            <div className='bg'>
+                <p>Browse file to upload </p>
+                <img src={cloud} alt="" />
+                <b>Supported Files</b>
+                <p>JPG, JPEG, PNG</p>
+            </div>
+          <p className='max'>Maximum of 4 pictures</p>
+        </div>
+        <div className='upload4'>
+            <form action="">
+                <div  className='formdives'>
+                    <div>
+                        <label htmlFor="text">Item Category</label><br/>
+                        <input type="text" placeholder='Price'/>   
+                    </div>
+                    <div>
+                        <label htmlFor="">Category</label><br/>
+                        <select name="" id="">
+                            <option value="Category">Category</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='desc'>
+                    <label htmlFor="">Description</label>
+                    <div>
+                        <input type="text" placeholder='Description' className='descs'/>
+                    </div>
+                   
+                </div>
+            </form>
+        </div>
+
+
+        <Footer/>
+    </>
+   
   )
 }
 
